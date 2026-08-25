8c2141baa4938f9fa8b2df1e376a739345cb37f8
Mahdi Amolimoghaddam <54014061+beaconchain-horizon@users.noreply.github.com>
2026-08-25 17:16:24 +0330

    ~/horizon-core-engine/

--- .conductor.sh
+++ .conductor.sh
@@ -1,11 +1,4 @@
-# 1. تغییر نام برنچ محلی از mine به main
 git branch -m mine main
-
-# 2. دریافت آخرین تغییرات از مخزن راه‌دور
 git fetch origin
-
-# 3. تنظیم برنچ main برای دنبال کردن origin/main
 git branch -u origin/main main
-
-# 4. تنظیم HEAD راه‌دور به برنچ main
 git remote set-head origin main
